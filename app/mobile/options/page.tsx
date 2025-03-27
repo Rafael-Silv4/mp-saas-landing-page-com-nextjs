5589a0915bff5cb329d1b177faa80b708825a8da
RafaelSillva <rafaelconceicao822@gmail.com>
2025-03-27 15:23:32 -0400

    projeto concluido

---
+++ app/mobile/options/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const OptionsMobile = () => {
+  const [mobi, setMobi] = useState(true);
+
+  useEffect(() => {
+    const isAvaliable = () => {
+      setMobi(window.innerWidth <= 430);
+    };
+
+    isAvaliable();
+    window.addEventListener("resize", isAvaliable);
+
+    return () => window.removeEventListener("resize", isAvaliable);
+  }, []);
+
+  if (!mobi) return null;
+
+  return (
+    <div>
+      <div>
+        <div className="flex justify-center items-center  ">
+          <h1 className=" mt-[10rem] text-[1.7rem] font-bold">
+            Como funciona?
+          </h1>
+        </div>
+        <div className=" flex justify-center items-center gap-5 h-[16rem] mb-[28rem]">
+          <Image
+            src="/woman.svg"
+            alt="woman"
+            width={100}
+            height={100}
+            className="w-[130px] h-[130px]"
+          />
+          <div className=" flex flex-col gap-4 text-[#6b7280] text-[12px]">
+            <p>Acesso a 1 ebook por mês</p>
+            <p>Curadoria especial</p>
+            <p>Cancele quando quiser</p>
+          </div>
+          <div className=" flex flex-col gap-7">
+            <Image src="/vector.png" alt="Check" width={15} height={15} />
+            <Image src="/vector.png" alt="Check" width={15} height={15} />
+            <Image src="/vector.png" alt="Check" width={15} height={15} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OptionsMobile;
